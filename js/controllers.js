8761e93b3b0ddaaad4bec7e5d585fca66520fb88
Ian MacDowell <Ian.MacDowell@rga.com>
2014-03-21 14:10:43 -0500

    clean up

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,10 @@
 
 angular.module('myApp.controllers', ['ngRoute']).
   controller('MyCtrl1', [ "$scope", "$http", function(scope, http) {
-  		console.log(scope);
   		scope.adjectives = [];
+  		scope.adjective = "";
   		scope.nouns = [];
-		scope.noun = "pickle";
-		scope.adjective = "smelly";
+		scope.noun = "";
 
 		http.get('data/adjectives.json')
 		  .then(function(result) {
@@ -21,17 +20,11 @@ angular.module('myApp.controllers', ['ngRoute']).
 		    scope.nouns = result.data.nouns;
 		    scope.noun = result.data.nouns[Math.floor(Math.random()*result.data.nouns.length)];	
 		});
+
+		scope.refresh = function(){
+			scope.adjective = scope.adjectives[Math.floor(Math.random()*scope.adjectives.length)];
+			scope.noun = scope.nouns[Math.floor(Math.random()*scope.nouns.length)];	
+		}  
+		
   }]);
 
-/*
-  'use strict';
-
-
-
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);*/
